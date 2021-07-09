b20fe4abc9c444dd8009a391476883b33f628dc6
danyalshah3 <72283767+danyalshah3@users.noreply.github.com>
2021-07-09 14:29:27 -0400

    commit tree 81b045fbb0526305bbb2967c87bf21b292d0ef83

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  root 'welcome#home'
+  get '/', to: 'welcome#home'
   # Add your routes here
+  match '/auth/:developer/callback', to: 'sessions#create', via: [:get, :post] 
 end
